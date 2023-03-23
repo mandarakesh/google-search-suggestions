e16e8a0c44e193aa1b91b7d92c650ca3fa0a0111
Rakesh <mandarakesh151@gmail.com>
2023-03-23 05:53:55 +0000

    first commit

---
+++ src/components/SuggestionItem/index.js
@@ -0,0 +1,23 @@
+import './index.css'
+
+const SuggestionItem = props => {
+  const {suggestionDetails, finalSearchInput} = props
+  const {suggestion} = suggestionDetails
+
+  const toSearchResult = () => {
+    finalSearchInput(suggestion)
+  }
+
+  return (
+    <li className="list-container">
+      <p className="para">{suggestion}</p>
+      <img
+        onClick={toSearchResult}
+        className="arrow-image"
+        alt="arrow"
+        src="https://assets.ccbp.in/frontend/react-js/diagonal-arrow-left-up.png"
+      />
+    </li>
+  )
+}
+export default SuggestionItem
